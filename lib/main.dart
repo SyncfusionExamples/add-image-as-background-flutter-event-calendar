5285bb17c0de851565b99e193610be646f364338
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-31 12:55:03 +0530

    FLUT-3276 - Sample modified and screenshots added.

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,37 @@ class Background_Image extends StatelessWidget {
                   image: DecorationImage(
                       image: AssetImage("icons/image.png"), fit: BoxFit.cover)),
               child: SfCalendar(
+                view: CalendarView.day,
+                allowedViews: <CalendarView>[
+                  CalendarView.day,
+                  CalendarView.week,
+                  CalendarView.workWeek,
+                  CalendarView.month,
+                  CalendarView.timelineDay,
+                  CalendarView.timelineWeek,
+                  CalendarView.timelineWorkWeek,
+                  CalendarView.timelineMonth,
+                  CalendarView.schedule
+                ],
                 backgroundColor: Colors.transparent,
                 cellBorderColor: Colors.white,
                 headerStyle: CalendarHeaderStyle(
-                    textStyle: TextStyle(color: Colors.white)),
+                    textStyle: TextStyle(color: Colors.blue)),
                 viewHeaderStyle: ViewHeaderStyle(
                     dayTextStyle: TextStyle(color: Colors.white),
-                    dateTextStyle: TextStyle(color:Colors.white)),
-                timeSlotViewSettings: TimeSlotViewSettings(timeTextStyle: TextStyle(color: Colors.white)),
+                    dateTextStyle: TextStyle(color: Colors.white)),
+                timeSlotViewSettings: TimeSlotViewSettings(
+                    timeTextStyle: TextStyle(color: Colors.white)),
+                monthViewSettings: MonthViewSettings(
+                    monthCellStyle: MonthCellStyle(
+                        textStyle: TextStyle(color: Colors.white),
+                        leadingDatesTextStyle: TextStyle(color: Colors.white),
+                        trailingDatesTextStyle:
+                            TextStyle(color: Colors.white))),
+                scheduleViewSettings: ScheduleViewSettings(
+                    monthHeaderSettings: MonthHeaderSettings(
+                        backgroundColor: Colors.transparent,
+                        monthTextStyle: TextStyle(color: Colors.blue))),
               )),
         )));
   }
